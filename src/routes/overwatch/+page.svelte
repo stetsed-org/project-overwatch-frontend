140d9903dbd3f0ddbe0bbd5a21f340107eaf271c
Stetsed <stetsed@protonmail.com>
2023-04-09 14:16:52 +0200

    Moved nav to seperate file

--- src/routes/overwatch/+page.svelte
+++ src/routes/overwatch/+page.svelte
@@ -1,22 +1,11 @@
 <script>
   import Form from './overwatch_form.svelte';
+  import Nav from '../../utils/nav.svelte';
 </script>
-<nav class="navbar flex items-center justify-between flex-wrap bg-gray-800 p-6">
-  <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <span class="font-semibold text-xl tracking-tight">TDEI Overwatch System
-    </span>
-  </div>
-  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-    <div class="text-sm lg:flex-grow">
-      <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Home
-      </a>
-      <a href="/overwatch" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-        Overwatch
-      </a>
-    </div>
-  </div>
-</nav>
+
+<div>
+  <Nav />
+</div>
 
 <div>
   <Form />
