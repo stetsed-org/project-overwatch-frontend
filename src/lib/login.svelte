1650c6bbef0c711f30fde0f12ff8e69fd6044a77
Stetsed <stetsed@protonmail.com>
2023-04-13 14:04:29 +0200

    Moved login screen slightly down

--- src/lib/login.svelte
+++ src/lib/login.svelte
@@ -16,24 +16,25 @@
     <button on:click={signOut}>Sign Out</button>
   </p>
 {:else}
+<div class="relative flex flex-col justify-center h-96 overflow-hidden">
 <form on:submit|preventDefault>
-  <div class="form-control w-full max-w-xs">
+  <div class="form-control w-full max-w-xs m-auto">
     <label class="label-text" for="username">
       Username
     </label>
     <input class="input input-bordered w-full max-w-xs" id="username" type="text" placeholder="Username" bind:value={username} required>
   </div>
-  <div class="form-control w-full max-w-xs">
+  <div class="form-control w-full max-w-xs m-auto">
     <label class="label-text" for="password">
       Password
     </label>
     <input class="input input-bordered w-full max-w-xs" id="password" type="password" placeholder="Password" bind:value={password} required>
   </div>
-  <div class="flex items-center justify-between">
+  <div class="form-control w-full max-w-xs m-auto">
     <button class="btn btn-md sm:btn-sm md:btn-md lg:btn-lg rounded" type="submit" on:click={login}>
       Login
     </button>
   </div>
 </form>
-
+</div>
 {/if}
