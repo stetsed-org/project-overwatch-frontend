9bc6654aec3139a2ac881f3e271db7a4d7b0e8f2
Stetsed <stetsed@protonmail.com>
2023-04-20 19:55:03 +0200

    Added map and time selection for map

--- src/routes/statistics/canvas.svelte
+++ src/routes/statistics/canvas.svelte
@@ -5,12 +5,13 @@
   let responseData: any[] = [];
   let formSubmitted: boolean = false;
   let username: string = "";
+  let startTime: string = "";
 
   let canvas: HTMLCanvasElement;
   let context: CanvasRenderingContext2D;
 
   const width = 1000;
-  const height = 490;
+  const height = 504;
   const maxXCoordinateValue = 12250;
   const maxZCoordinateValue = 6000;
 
@@ -32,7 +33,7 @@
 
       context.beginPath();
       context.arc(xx, yy, 2, 0, 2 * Math.PI);
-      context.fillStyle = 'red';
+      context.fillStyle = 'black';
       context.fill();
       context.closePath();
     });
@@ -41,7 +42,10 @@
   async function fetchData() {
     event.preventDefault();
     try {
-      const filter = `account ~ '${username}'`;
+      let filter = `account ~ '${username}'`;
+      if (startTime) {
+        filter += ` && created > '${startTime}'`;
+      }
       const nestedData = await pb.collection('global').getFullList({
           filter: filter,
         });
@@ -61,10 +65,6 @@
   const handleChangeName = () => {
       formSubmitted = false;
     };
-
-  onMount(() => {
-    context = canvas.getContext('2d');
-  });
 </script>
 
 {#if !formSubmitted}
@@ -75,14 +75,20 @@
       </label>
       <input type="text" id="username" class="input input-bordered w-full max-w-xs" placeholder="Username" bind:value={username} />
     </div>
+    <div class="form-control w-full max-w-xs">
+      <label class="label" for="startTime">
+        <span class="label-text">Start Time(Optional)</span>
+      </label>
+      <input type="datetime-local" id="startTime" class="input input-bordered w-full max-w-xs" bind:value={startTime}/>
+    </div>
     <button type="submit" class="btn btn-md sm:btm-sm md:btm-md lg:btn-lg" >Submit</button>
   </form>
 {:else}
-<div class="container mx-auto p-4">
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4" on:click={handleChangeName}>Change Username</button>
+<div class="container mx-auto p-4 justify-center">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4" on:click={handleChangeName}>Change Information</button>
     {#if responseData.length > 0}
       {#if formSubmitted}
-        <div class="bg-white">
+        <div class="bg-[url('map.png')] w-[1000px]">
           <canvas bind:this={canvas} width={width} height={height} style="border: 1px solid;"></canvas>
         </div>
       {/if} 
