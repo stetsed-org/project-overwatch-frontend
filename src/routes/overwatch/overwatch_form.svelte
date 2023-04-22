f6f0242ca8cdc423d3d56baaf49dbfcffebd6bae
Stetsed <stetsed@protonmail.com>
2023-04-22 10:49:12 +0200

    Minor Formatting Changes

--- src/routes/overwatch/overwatch_form.svelte
+++ src/routes/overwatch/overwatch_form.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { currentUser, pb} from "../../lib/pocketbase.ts";
+  import { onMount } from "svelte";
+  import { currentUser, pb } from "../../lib/pocketbase.ts";
   let xCoordinate: number;
   let zCoordinate: number;
   let startTime: string;
@@ -10,7 +10,7 @@
   let responseDataa: any[] = [];
 
   onMount(async () => {
-    const resultListt = await pb.collection('global_amount').getList(1, 1);
+    const resultListt = await pb.collection("global_amount").getList(1, 1);
     responseDataa = resultListt.items;
 
     for (let i = 0; i < responseDataa.length; i++) {
@@ -19,8 +19,10 @@
   });
 
   const handleSubmit = async (event: Event) => {
-    const filter = `x > ${xCoordinate - 20} && x < ${xCoordinate + 20} && z > ${zCoordinate - 20} && z < ${zCoordinate + 20} && created > "${startTime}"`;
-    responseData = await pb.collection('global').getFullList({
+    const filter = `x > ${xCoordinate - 20} && x < ${xCoordinate + 20} && z > ${
+      zCoordinate - 20
+    } && z < ${zCoordinate + 20} && created > "${startTime}"`;
+    responseData = await pb.collection("global").getFullList({
       filter: filter,
     });
     formSubmitted = true;
@@ -32,10 +34,21 @@
 </script>
 
 {#if !formSubmitted}
-  <div class="stats shadow">  
+  <div class="stats shadow">
     <div class="stat">
       <div class="stat-figure text-secondary">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="inline-block w-8 h-8 stroke-current"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 10V3L4 14h7v7l9-11h-7z"
+          /></svg
+        >
       </div>
       <div class="stat-title">Amount of Entries</div>
       <div class="stat-value text-secondary">{amount_of_records}</div>
@@ -46,25 +59,51 @@
       <label class="label" for="xCoordinate">
         <span class="label-text">X-Coordinate</span>
       </label>
-      <input type="number" id="xCoordinate" placeholder="Enter X Coordinate" class="input input-bordered w-full max-w-xs" bind:value={xCoordinate} required />
+      <input
+        type="number"
+        id="xCoordinate"
+        placeholder="Enter X Coordinate"
+        class="input input-bordered w-full max-w-xs"
+        bind:value={xCoordinate}
+        required
+      />
     </div>
     <div class="form-control w-full max-w-xs">
       <label class="label" for="zCoordinate">
         <span class="label-text">Z-Coordinate</span>
       </label>
-      <input type="number" id="zCoordinate" placeholder="Enter Z Coordinate" class="input input-bordered w-full max-w-xs" bind:value={zCoordinate} required />
+      <input
+        type="number"
+        id="zCoordinate"
+        placeholder="Enter Z Coordinate"
+        class="input input-bordered w-full max-w-xs"
+        bind:value={zCoordinate}
+        required
+      />
     </div>
     <div class="form-control w-full max-w-xs">
       <label class="label" for="startTime">
         <span class="label-text">Start Time</span>
       </label>
-      <input type="datetime-local" id="startTime" class="input input-bordered w-full max-w-xs" bind:value={startTime} required />
+      <input
+        type="datetime-local"
+        id="startTime"
+        class="input input-bordered w-full max-w-xs"
+        bind:value={startTime}
+        required
+      />
     </div>
-    <button class="btn btn-md sm:btn-sm md:btn-md lg:btn-lg rounded" type="submit">Submit</button>
+    <button
+      class="btn btn-md sm:btn-sm md:btn-md lg:btn-lg rounded"
+      type="submit">Submit</button
+    >
   </form>
 {:else}
   <div class="container mx-auto p-4">
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4" on:click={handleChangeCoordinates}>Change Coordinates</button>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+      on:click={handleChangeCoordinates}>Change Coordinates</button
+    >
     {#if responseData.length > 0}
       <table class="table w-full">
         <thead>
--- src/routes/statistics/canvas.svelte
+++ src/routes/statistics/canvas.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { currentUser, pb} from "../../lib/pocketbase.ts";
+  import { onMount } from "svelte";
+  import { currentUser, pb } from "../../lib/pocketbase.ts";
 
   let responseData: any[] = [];
   let formSubmitted: boolean = false;
@@ -24,7 +24,7 @@
   }
 
   function plotCoordinates() {
-    context = canvas.getContext('2d');
+    context = canvas.getContext("2d");
     context.clearRect(0, 0, width, height);
 
     responseData.forEach(({ x, z, ..._ }) => {
@@ -33,7 +33,7 @@
 
       context.beginPath();
       context.arc(xx, yy, 2, 0, 2 * Math.PI);
-      context.fillStyle = 'black';
+      context.fillStyle = "black";
       context.fill();
       context.closePath();
     });
@@ -46,25 +46,25 @@
       if (startTime) {
         filter += ` && created > '${startTime}'`;
       }
-      const nestedData = await pb.collection('global').getFullList({
-          filter: filter,
-        });
+      const nestedData = await pb.collection("global").getFullList({
+        filter: filter,
+      });
       responseData = nestedData.flat();
       console.log(responseData);
       formSubmitted = true;
     } catch (error) {
-      console.error('Failed to fetch data:', error);
+      console.error("Failed to fetch data:", error);
     }
   }
 
   $: if (formSubmitted && canvas) {
-    context = canvas.getContext('2d');
+    context = canvas.getContext("2d");
     plotCoordinates();
   }
 
   const handleChangeName = () => {
-      formSubmitted = false;
-    };
+    formSubmitted = false;
+  };
 </script>
 
 {#if !formSubmitted}
@@ -73,28 +73,50 @@
       <label class="label" for="username">
         <span class="label-text">Username</span>
       </label>
-      <input type="text" id="username" class="input input-bordered w-full max-w-xs" placeholder="Username" bind:value={username} />
+      <input
+        type="text"
+        id="username"
+        class="input input-bordered w-full max-w-xs"
+        placeholder="Username"
+        bind:value={username}
+      />
     </div>
     <div class="form-control w-full max-w-xs">
       <label class="label" for="startTime">
         <span class="label-text">Start Time(Optional)</span>
       </label>
-      <input type="datetime-local" id="startTime" class="input input-bordered w-full max-w-xs" bind:value={startTime}/>
+      <input
+        type="datetime-local"
+        id="startTime"
+        class="input input-bordered w-full max-w-xs"
+        bind:value={startTime}
+      />
     </div>
-    <button type="submit" class="btn btn-md sm:btm-sm md:btm-md lg:btn-lg" >Submit</button>
+    <button type="submit" class="btn btn-md sm:btm-sm md:btm-md lg:btn-lg"
+      >Submit</button
+    >
   </form>
 {:else}
-<div class="container mx-auto p-4 justify-center">
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4" on:click={handleChangeName}>Change Information</button>
+  <div class="container mx-auto p-4 justify-center">
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+      on:click={handleChangeName}>Change Information</button
+    >
     {#if responseData.length > 0}
       {#if formSubmitted}
-        <div class="bg-[url('https://cdn.selfhostable.net/map.png')] w-[1000px]">
-          <canvas bind:this={canvas} width={width} height={height} style="border: 1px solid;"></canvas>
+        <div
+          class="bg-[url('https://cdn.selfhostable.net/map.png')] w-[1000px]"
+        >
+          <canvas
+            bind:this={canvas}
+            {width}
+            {height}
+            style="border: 1px solid;"
+          />
         </div>
-      {/if} 
+      {/if}
     {:else}
       <p class="text-center text-gray-500">No data found</p>
     {/if}
-</div>
+  </div>
 {/if}
-
