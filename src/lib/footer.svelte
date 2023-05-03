68aa22a595e7d84643c8d1f136e6af5eb1f6089f
Stetsed <stetsed@protonmail.com>
2023-05-03 11:21:46 +0200

    Updated Footer

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,7 +1,18 @@
-<footer class="footer footer-center p-4 bg-base-300 text-base-content fixed inset-x-0 bottom-0">
+<footer
+  class="footer footer-center p-4 bg-base-300 text-base-content fixed inset-x-0 bottom-0"
+>
   <div>
-    <p>Made by Stetsed, with <a class="link" href="https://svelte.dev">Svelte</a> and <a class="link" href="https://tailwindcss.com">Tailwind</a></p>
-    <p>Source code is available on <a class="link" href="https://github.com/Stetsed/project-overwatch-frontend">GitHub</a></p>
+    <p>
+      Made by Stetsed, with <a class="link" href="https://svelte.dev">Svelte</a>
+      and <a class="link" href="https://tailwindcss.com">Tailwind</a>
+    </p>
+    <p>
+      Source code is available on <a
+        class="link"
+        href="https://github.com/Stetsed-org/project-overwatch-frontend"
+        >GitHub</a
+      >
+    </p>
     <p>Copyright © 2023 - All right reserved by Stetsed</p>
   </div>
 </footer>
