565cae6f6671b7291801fba466d2df76c18f686b
Stetsed <stetsed@protonmail.com>
2023-05-10 12:55:32 +0200

    Made POCKETBASE_URL dynamic

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,12 +1,13 @@
 import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
+import { env } from '$env/dynamic/public'
 
-export const pb = new PocketBase('https://pocketbase.selfhostable.net'); // remote
+export const pb = new PocketBase(env.PUBLIC_POCKETBASE_URL); // remote
 
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange((auth) => {
-    console.log('authStore changed', auth);
-    currentUser.set(pb.authStore.model);
+  console.log('authStore changed', auth);
+  currentUser.set(pb.authStore.model);
 });
